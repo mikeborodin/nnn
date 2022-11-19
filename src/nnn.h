445c6b1884b5322e86bb6cc586460a851a4720bc
mikeborodin <mike@mikeborodin.dev>
2022-11-19 13:46:34 +0100

    [edit] keymapping to match my preferences on colemak keyboard

--- src/nnn.h
+++ src/nnn.h
@@ -33,7 +33,7 @@
 #include <curses.h>
 #include <wchar.h>
 
-#define CONTROL(c) ((c) & 0x1f)
+#define CONTROL(c) ((c)&0x1f)
 
 #ifndef ESC
 #define ESC (27)
@@ -44,7 +44,8 @@
 #endif
 
 /* Supported actions */
-enum action {
+enum action
+{
 	SEL_BACK = 1,
 	SEL_OPEN,
 	SEL_NAV_IN,
@@ -122,165 +123,185 @@ enum action {
 };
 
 /* Associate a pressed key to an action */
-struct key {
-	wint_t sym;      /* Key pressed */
+struct key
+{
+	wint_t sym;		 /* Key pressed */
 	enum action act; /* Action */
 };
 
+// Right
+// hot actoins = lyhosk;'
+// secondary = []09
+
+// Left
+// hot actions = rst
+// 				wfpgvb/
+// secondary = `zcx
+//
+// Done
+// arrows = ueni
+// d = delete
+// a = new
+// q = quit
+// ~ = home
+//
+
 static struct key bindings[] = {
 	/* Back */
-	{ KEY_LEFT,       SEL_BACK },
-	{ 'h',            SEL_BACK },
+	{KEY_LEFT, SEL_BACK},
+	{'n', SEL_BACK},
 	/* Inside or select */
-	{ KEY_ENTER,      SEL_OPEN },
-	{ '\r',           SEL_OPEN },
+	{KEY_ENTER, SEL_OPEN},
+	{'\r', SEL_OPEN},
 	/* Pure navigate inside */
-	{ KEY_RIGHT,      SEL_NAV_IN },
-	{ 'l',            SEL_NAV_IN },
+	{KEY_RIGHT, SEL_NAV_IN},
+	{'i', SEL_NAV_IN},
 	/* Next */
-	{ 'j',            SEL_NEXT },
-	{ KEY_DOWN,       SEL_NEXT },
+	{'e', SEL_NEXT},
+	{KEY_DOWN, SEL_NEXT},
 	/* Previous */
-	{ 'k',            SEL_PREV },
-	{ KEY_UP,         SEL_PREV },
-	/* Page down */
-	{ KEY_NPAGE,      SEL_PGDN },
-	/* Page up */
-	{ KEY_PPAGE,      SEL_PGUP },
-	/* Ctrl+D */
-	{ CONTROL('D'),   SEL_CTRL_D },
-	/* Ctrl+U */
-	{ CONTROL('U'),   SEL_CTRL_U },
-	/* First entry */
-	{ KEY_HOME,       SEL_HOME },
-	{ 'g',            SEL_HOME },
-	{ CONTROL('A'),   SEL_HOME },
-	/* Last entry */
-	{ KEY_END,        SEL_END },
-	{ 'G',            SEL_END },
-	{ CONTROL('E'),   SEL_END },
-	/* Go to first file */
-	{ '\'',           SEL_FIRST },
-	/* Jump to an entry number/offset */
-	{ 'J',            SEL_JUMP },
+	{'u', SEL_PREV},
+	{KEY_UP, SEL_PREV},
 	/* HOME */
-	{ '~',            SEL_CDHOME },
-	/* Initial directory */
-	{ '@',            SEL_CDBEGIN },
-	/* Last visited dir */
-	{ '-',            SEL_CDLAST },
-	/* Go to / */
-	{ '`',            SEL_CDROOT },
-	/* Leader key */
-	{ 'b',            SEL_BMOPEN },
-	{ CONTROL('_'),   SEL_BMOPEN },
-	/* Connect to server over SSHFS */
-	{ 'c',            SEL_REMOTE },
-	/* Cycle contexts in forward direction */
-	{ '\t',           SEL_CYCLE },
-	/* Cycle contexts in reverse direction */
-	{ KEY_BTAB,       SEL_CYCLER },
+	{'~', SEL_CDHOME},
 	/* Go to/create context N */
-	{ '1',            SEL_CTX1 },
-	{ '2',            SEL_CTX2 },
-	{ '3',            SEL_CTX3 },
-	{ '4',            SEL_CTX4 },
+	{'1', SEL_CTX1},
+	{'2', SEL_CTX2},
+	{'3', SEL_CTX3},
+	{'4', SEL_CTX4},
 #ifdef CTX8
-	{ '5',            SEL_CTX5 },
-	{ '6',            SEL_CTX6 },
-	{ '7',            SEL_CTX7 },
-	{ '8',            SEL_CTX8 },
+	{'5', SEL_CTX5},
+	{'6', SEL_CTX6},
+	{'7', SEL_CTX7},
+	{'8', SEL_CTX8},
 #endif
-	/* Mark a path to visit later */
-	{ ',',            SEL_MARK },
-	/* Create a bookmark */
-	{ 'B',            SEL_BMARK },
-	/* Filter */
-	{ '/',            SEL_FLTR },
-	/* Toggle filter mode */
-	{ CONTROL('N'),   SEL_MFLTR },
-	/* Toggle hide .dot files */
-	{ '.',            SEL_HIDDEN },
-	/* Detailed listing */
-	{ 'd',            SEL_DETAIL },
-	/* File details */
-	{ 'f',            SEL_STATS },
-	{ CONTROL('F'),   SEL_STATS },
-	/* Toggle executable status */
-	{ '*',            SEL_CHMODX },
-	/* Create archive */
-	{ 'z',            SEL_ARCHIVE },
-	/* Sort toggles */
-	{ 't',            SEL_SORT },
-	{ CONTROL('T'),   SEL_SORT },
-	/* Redraw window */
-	{ CONTROL('L'),   SEL_REDRAW },
-	/* Select current file path */
-	{ ' ',            SEL_SEL },
-	{ '+',            SEL_SEL },
-	/* Toggle select multiple files */
-	{ 'm',            SEL_SELMUL },
-	/* Select all files in current dir */
-	{ 'a',            SEL_SELALL },
-	/* Invert selection in current dir */
-	{ 'A',            SEL_SELINV },
-	/* List, edit selection */
-	{ 'E',            SEL_SELEDIT },
 	/* Copy from selection buffer */
-	{ 'p',            SEL_CP },
-	{ CONTROL('P'),   SEL_CP },
+	{'p', SEL_CP},
+	{CONTROL('P'), SEL_CP},
 	/* Move from selection buffer */
-	{ 'v',            SEL_MV },
-	{ CONTROL('V'),   SEL_MV },
-	/* Copy/move from selection buffer and rename */
-	{ 'w',            SEL_CPMVAS },
-	{ CONTROL('W'),   SEL_CPMVAS },
+	{'m', SEL_MV},
+	{CONTROL('M'), SEL_MV},
+	/* Filter */
+	{'f', SEL_FLTR},
+	/* Toggle filter mode */
+	{CONTROL('F'), SEL_MFLTR},
+	/* Toggle hide .dot files */
+	{'.', SEL_HIDDEN},
 	/* Delete from selection buffer */
-	{ 'x',            SEL_RM },
-	{ CONTROL('X'),   SEL_RM },
-	/* Open in a custom application */
-	{ 'o',            SEL_OPENWITH },
-	{ CONTROL('O'),   SEL_OPENWITH },
-	/* Create a new file */
-	{ 'n',            SEL_NEW },
-	/* Show rename prompt */
-	{ CONTROL('R'),   SEL_RENAME },
-	/* Rename contents of current dir */
-	{ 'r',            SEL_RENAMEMUL },
-	/* Disconnect a SSHFS mount point */
-	{ 'u',            SEL_UMOUNT },
-	/* Show help */
-	{ '?',            SEL_HELP },
-	/* Toggle auto-advance on file open */
-	{ CONTROL('J'),   SEL_AUTONEXT },
-	/* Edit in EDITOR */
-	{ 'e',            SEL_EDIT },
-	/* Run a plugin */
-	{ ';',            SEL_PLUGIN },
+	{'d', SEL_RM},
+	{CONTROL('D'), SEL_RM},
 	/* Run command */
-	{ '!',            SEL_SHELL },
-	{ CONTROL(']'),   SEL_SHELL },
-	/* Launcher */
-	{ '=',            SEL_LAUNCH },
-	/* Show command prompt */
-	{ ']',            SEL_PROMPT },
-	/* Lock screen */
-	{ '0',            SEL_LOCK },
-	/* Manage sessions */
-	{ 's',            SEL_SESSIONS },
-	/* Export list */
-	{ '>',            SEL_EXPORT },
-	/* Set time type */
-	{ 'T',            SEL_TIMETYPE },
-	/* Quit a context */
-	{ 'q',            SEL_QUITCTX },
-	/* Change dir on quit */
-	{ CONTROL('G'),   SEL_QUITCD },
-	/* Quit */
-	{ CONTROL('Q'),   SEL_QUIT },
-	/* Quit with an error code */
-	{ 'Q',            SEL_QUITERR },
+	{'w', SEL_SHELL},
 #ifndef NOMOUSE
-	{ KEY_MOUSE,      SEL_CLICK },
+	{KEY_MOUSE, SEL_CLICK},
 #endif
+	/* Select current file path */
+	{' ', SEL_SEL},
+	/* Sort toggles */
+	{']', SEL_SORT},
+	/* Detailed listing */
+	{'\\', SEL_DETAIL},
+	/* Edit in EDITOR */
+	{'/', SEL_EDIT},
+	/* Toggle executable status */
+	{'*', SEL_CHMODX},
+	/* Create archive */
+	{'z', SEL_ARCHIVE},
+	/* Quit a context */
+	{'q', SEL_QUITCTX},
+	/* Create a new file */
+	{'a', SEL_NEW},
+	/* Show rename prompt */
+	{'r', SEL_RENAME},
+	/* Rename contents of current dir */
+	//? {'R', SEL_RENAMEMUL},
+	/* Select all files in current dir */
+	{'X', SEL_SELINV},
+	/* Invert selection in current dir */
+	{'A', SEL_SELALL},
+	/* Redraw window */
+	{ 's',   SEL_REDRAW },
+	{ 'h',   SEL_BMOPEN },
+
+    /* File details */
+	// { 'f',            SEL_STATS },
+	// { CONTROL(']'),   SEL_SHELL },
+	/* Page down */
+	// { KEY_NPAGE,      SEL_PGDN },
+	/* Page up */
+	// { KEY_PPAGE,      SEL_PGUP },
+	/* Ctrl+D */
+	// { CONTROL('D'),   SEL_CTRL_D },
+	/* Ctrl+U */
+	// { CONTROL('U'),   SEL_CTRL_U },
+	/* First entry */
+	// { KEY_HOME,       SEL_HOME },
+	// { 'g',            SEL_HOME },
+	// { CONTROL('A'),   SEL_HOME },
+	/* Last entry */
+	// { KEY_END,        SEL_END },
+	// { 'G',            SEL_END },
+	// { CONTROL('E'),   SEL_END },
+	/* Go to first file */
+	// { '\'',           SEL_FIRST },
+	/* Jump to an entry number/offset */
+	// { 'J',            SEL_JUMP },
+	/* Initial directory */
+	// { '@',            SEL_CDBEGIN },
+	/* Last visited dir */
+	// { '-',            SEL_CDLAST },
+	/* Go to / */
+	// { '`',            SEL_CDROOT },
+	/* Leader key */
+	// { 'b',            SEL_BMOPEN },
+	// { CONTROL('_'),   SEL_BMOPEN },
+	/* Connect to server over SSHFS */
+	// { 'c',            SEL_REMOTE },
+	/* Cycle contexts in forward direction */
+	// { '\t',           SEL_CYCLE },
+	/* Cycle contexts in reverse direction */
+	{KEY_BTAB, SEL_CYCLER},
+	/* Mark a path to visit later */
+	{',', SEL_MARK},
+	/* Create a bookmark */
+	{'B', SEL_BMARK},
+	// { CONTROL('F'),   SEL_STATS },
+	// { CONTROL('T'),   SEL_SORT },
+	// {'+', SEL_SEL},
+	/* Toggle select multiple files */
+	{'m', SEL_SELMUL},
+	/* List, edit selection */
+	{'E', SEL_SELEDIT},
+	/* Copy/move from selection buffer and rename */
+	{'w', SEL_CPMVAS},
+	{CONTROL('W'), SEL_CPMVAS},
+	/* Open in a custom application */
+	{'o', SEL_OPENWITH},
+	{CONTROL('O'), SEL_OPENWITH},
+	/* Disconnect a SSHFS mount point */
+	{'k', SEL_UMOUNT},
+	/* Show help */
+	{'?', SEL_HELP},
+	/* Toggle auto-advance on file open */
+	{CONTROL('J'), SEL_AUTONEXT},
+	/* Run a plugin */
+	{';', SEL_PLUGIN},
+	/* Launcher */
+	{'=', SEL_LAUNCH},
+	/* Show command prompt */
+	{']', SEL_PROMPT},
+	/* Lock screen */
+	{'0', SEL_LOCK},
+	/* Manage sessions */
+	// {'s', SEL_SESSIONS},
+	/* Export list */
+	{'>', SEL_EXPORT},
+	/* Set time type */
+	{'T', SEL_TIMETYPE},
+	/* Change dir on quit */
+	{CONTROL('G'), SEL_QUITCD},
+	/* Quit */
+	// { CONTROL('Q'),   SEL_QUIT },
+	/* Quit with an error code */
+	// { 'Q',            SEL_QUITERR },
 };
